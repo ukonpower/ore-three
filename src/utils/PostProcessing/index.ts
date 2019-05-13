adde208d17b615edb74c22a7316e3ac1c42b6ec5
ukonpower <>
2019-05-13 13:13:47 +0900

    edit postprocessing

--- src/utils/PostProcessing/index.ts
+++ src/utils/PostProcessing/index.ts
@@ -2,11 +2,11 @@ import * as THREE from 'three';
 import ppVert from './shaders/post-processing.vs';
 
 declare interface PPParam {
-    fragment: string,
+    fragmentShader: string,
     uniforms: any
 }
 
-declare interface EffectMaterials{
+declare interface EffectMaterials {
     material: THREE.ShaderMaterial,
     uniforms: any
 }
@@ -35,27 +35,27 @@ export class PostProcessing {
         parameter.forEach((param, i) => {
             let mat = new THREE.ShaderMaterial({
                 uniforms: param.uniforms,
-                vertexShader: ppVert,
-                fragmentShader: param.fragment,
+                vertexShader: "varying vec2 vUv; void main() { vUv = uv; gl_Position = vec4( position, 1.0 ); } ",
+                fragmentShader: param.fragmentShader,
             })
-            let effectMaterial = {material:mat,uniforms:param.uniforms}
-            if(!this.effectMaterials){
+            param.uniforms.backbuffer = { vlaue: null };
+            let effectMaterial = { material: mat, uniforms: param.uniforms }
+            if (!this.effectMaterials) {
                 this.effectMaterials = [effectMaterial];
             }
             this.effectMaterials.push(effectMaterial);
         });
     }
 
-    private initRenderTargets(){
-        let size:THREE.Vector2 = new THREE.Vector2();
+    private initRenderTargets() {
+        let size: THREE.Vector2 = new THREE.Vector2();
         this.renderer.getSize(size);
         size.multiplyScalar(this.renderer.getPixelRatio());
-        console.log(this.renderer)
-        this.readBuffer = new THREE.WebGLRenderTarget(size.x,size.y);
-        this.writeBuffer = new THREE.WebGLRenderTarget(size.x,size.y);
+        this.readBuffer = new THREE.WebGLRenderTarget(size.x, size.y);
+        this.writeBuffer = new THREE.WebGLRenderTarget(size.x, size.y);
     }
 
-    private swapBuffers(){
+    private swapBuffers() {
         let tmp = this.readBuffer;
         this.readBuffer = this.writeBuffer;
         this.writeBuffer = tmp;
@@ -63,18 +63,18 @@ export class PostProcessing {
 
     render(scene: THREE.Scene, camera: THREE.Camera) {
         this.renderer.setRenderTarget(this.readBuffer);
-        this.renderer.render(scene,camera);
-        this.effectMaterials.forEach((mat,i)=>{
+        this.renderer.render(scene, camera);
+        this.effectMaterials.forEach((mat, i) => {
             this.screenMesh.material = mat.material;
             mat.uniforms["backbuffer"].value = this.readBuffer.texture;
 
-            if(i < this.effectMaterials.length - 1){
+            if (i < this.effectMaterials.length - 1) {
                 this.renderer.setRenderTarget(this.writeBuffer);
-            }else{
+            } else {
                 this.renderer.setRenderTarget(null);
             }
 
-            this.renderer.render(this.scene,this.camera);
+            this.renderer.render(this.scene, this.camera);
             this.swapBuffers();
         })
     }
