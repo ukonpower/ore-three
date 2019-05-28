40758d45b45e5f855b80392386a30c260f32a5d1
ukonpower <obshi0120@icloud.com>
2019-05-28 16:30:29 +0900

    0.0.9

--- types/core/Controller.d.ts
+++ types/core/Controller.d.ts
@@ -1,10 +1,15 @@
 import * as THREE from 'three';
 import * as ORE from '../scene/BaseScene';
+export declare interface OreControllerParam {
+    canvas: HTMLCanvasElement;
+    retina?: boolean;
+    alpha?: boolean;
+}
 export declare class Controller {
     private canvas;
     private currentScene;
     renderer: THREE.WebGLRenderer;
-    constructor(canvas: any, retina: boolean);
+    constructor(parameter: OreControllerParam);
     private animate;
     setScene(scene: ORE.BaseScene): void;
     private onWindowResize;
