b6aee5edb2bddd0fd026f07edb1732120af29c71
ukonpower <ukon.ryohei@junni.jp>
2019-06-07 14:16:55 +0900

    create DOM in script

--- examples/js/scenes/DomGLSLScene.js
+++ examples/js/scenes/DomGLSLScene.js
@@ -1,52 +1,66 @@
 import * as ORE from '../../../src/';
 import * as THREE from 'three';
-import frag from './glsl/dom-glsl.fs'
+import frag from './glsl/dom-glsl.fs';
 
 export default class DomGLSLScene extends ORE.BaseScene {
-	constructor(renderer) {
-		super(renderer);
+
+	constructor( renderer ) {
+
+		super( renderer );
 		this.name = "DomGLSLScene";
 		this.init();
+
 	}
 
 	init() {
-		this.camera.position.set(0, 1.5, 3);
-		this.camera.lookAt(0, 0, 0);
+
+		this.camera.position.set( 0, 1.5, 3 );
+		this.camera.lookAt( 0, 0, 0 );
+
+		let dom = document.createElement('div');
+		dom.classList.add('dom-glsl');
+		document.body.insertBefore(dom,document.querySelector('#canvas'));
 
 		//create domGLSL
 		this.param = {
-			dom: document.querySelector('.dom-glsl'),
+			dom: dom,
 			fragmentShader: frag,
 			uniforms: {
-				time:{ value : 0 } 
+				time: { value: 0 }
 			}
-		}
+		};
 
-		this.domglsl = new ORE.DomGLSL(this.param)
-		
-		this.scene.add(this.domglsl);
+		this.domglsl = new ORE.DomGLSL( this.param );
+
+		this.scene.add( this.domglsl );
+
 	}
 
 	animate() {
+
 		//update dom size and position
 		this.domglsl.updateDom();
 
 		//update uniform
 		this.param.uniforms.time.value = this.time;
 
-		this.renderer.render(this.scene, this.camera);
+		this.renderer.render( this.scene, this.camera );
+
 	}
 
-	onResize(width, height) {
-		super.onResize(width, height);
+	onResize( width, height ) {
+
+		super.onResize( width, height );
+
 	}
 
-	onTouchStart(e) {
+	onTouchStart( e ) {
 	}
 
-	onTouchMove(e) {}
+	onTouchMove( e ) {}
 
-	onTouchEnd(e) {}
+	onTouchEnd( e ) {}
 
-	onWheel(e) {}
+	onWheel( e ) {}
+
 }
--- examples/js/scenes/PageScrollerScene.js
+++ examples/js/scenes/PageScrollerScene.js
@@ -2,30 +2,50 @@ import * as ORE from '../../../src/';
 import * as THREE from 'three';
 
 export default class MainScene extends ORE.BaseScene {
+
 	constructor(renderer) {
+	
 		super(renderer);
+	
 		this.name = "MainScene";
 		this.init();
+	
 	}
 
 	init() {
+	
 		this.camera.position.set(0, 1.5, 3);
 		this.camera.lookAt(0, 0, 0);
 
-		this.scroller = new ORE.PageScroller(document.querySelector('.wrapper'));
+		let wrapper = document.createElement('div');
+		wrapper.classList.add('wrapper');
+		document.body.appendChild(wrapper);
+
+		this.target = document.createElement('div');
+		this.target.classList.add('target');
+		wrapper.appendChild(this.target);
+
+		this.scroller = new ORE.PageScroller(wrapper);
+	
 	}
 
 	animate() {
+	
 		this.scroller.update(this.deltaTime);
 		this.renderer.render(this.scene, this.camera);
+	
 	}
 
 	onResize(width, height) {
+	
 		super.onResize(width, height);
+	
 	}
 
 	onTouchStart(e) {
-		this.scroller.moveto(document.querySelector('.dom-glsl'));
+	
+		this.scroller.moveto(this.target);
+	
 	}
 
 	onTouchMove(e) {
@@ -35,6 +55,8 @@ export default class MainScene extends ORE.BaseScene {
 	}
 
 	onWheel(e){
+		
 		this.scroller.setScrollVelocity(e.deltaY)
+
 	}
 }
