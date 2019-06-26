15c69df6481a642e9e72e353cff5cf43abbe6bd0
ukonpower <obshi0120@icloud.com>
2019-06-26 17:07:06 +0900

    update GPUComputationController

--- src/utils/GPUComputationController/index.ts
+++ src/utils/GPUComputationController/index.ts
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import { Uniforms } from '../../shaders/shader';
 
 import passThroughVert from './shaders/passThrough.vs';
+import passThroughFrag from './shaders/passThrough.fs';
 
 export interface GPUComputationKernel{
     material: THREE.RawShaderMaterial,
@@ -21,6 +22,7 @@ export class GPUComputationController {
     private camera: THREE.Camera;
 
     private mesh: THREE.Mesh;
+    private materials: THREE.ShaderMaterial[];
 
     private tempData: GPUcomputationData;
 
@@ -44,12 +46,23 @@ export class GPUComputationController {
 		this.camera = new THREE.Camera();
         this.camera.position.z = 1;
         
+        this.materials = [];
         this.mesh = new THREE.Mesh( new THREE.PlaneBufferGeometry( 2, 2 ) );
 		this.scene.add( this.mesh );
 
     }
 
-    public createData(): GPUcomputationData{
+    public createInitializeTexture(): THREE.DataTexture {
+
+		let a = new Float32Array( this.resolution.x * this.resolution.y * 4 );
+		let texture = new THREE.DataTexture( a, this.resolution.x, this.resolution.y, THREE.RGBAFormat, THREE.FloatType );
+		texture.needsUpdate = true;
+
+		return texture;
+
+	}
+
+    public createData( initializeTexture?: THREE.DataTexture ): GPUcomputationData{
 
         let buf = new THREE.WebGLRenderTarget( this.resolution.x, this.resolution.y, {
             wrapS: THREE.ClampToEdgeWrapping,
@@ -62,7 +75,19 @@ export class GPUComputationController {
 			depthBuffer: false
         });
 
-        return { buffer: buf };
+        let data = { buffer: buf };
+
+        if( initializeTexture ){
+
+            let initKernel = this.createKernel( passThroughFrag );
+
+            initKernel.uniforms.texture = { value: initializeTexture };
+
+            this.compute( initKernel, data );
+
+        }
+
+        return data;
 
     }
 
@@ -79,6 +104,8 @@ export class GPUComputationController {
             fragmentShader: shader,
             uniforms: uniforms
         });
+
+        this.materials.push(mat);
 
         let kernel: GPUComputationKernel = {
             material: mat,
@@ -108,6 +135,23 @@ export class GPUComputationController {
         let tmp = b1.buffer;
         b1.buffer = b2.buffer;
         b2.buffer = tmp;
+
+    }
+
+    public dispose(){
+
+        let geo = this.mesh.geometry;
+        geo.dispose();
+        
+        for( let i = 0; i < this.materials.length; i++ ){
+
+            this.materials[i].dispose();
+        
+        }
+
+        this.scene.remove( this.mesh );
+
+        this.tempData.buffer.dispose();
 
     }
 
