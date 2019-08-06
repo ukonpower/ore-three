2e45239b7a7e09e72b1933e2bdf12a1f40928d4a
ukonpower <obshi0120@icloud.com>
2019-08-06 23:18:54 +0900

    update easing scene

--- examples/js/scenes/EasingScene.js
+++ examples/js/scenes/EasingScene.js
@@ -18,14 +18,14 @@ export default class MainScene extends ORE.BaseScene {
 
 		this.renderer = this.gProps.renderer;
 
-		this.camera.position.set( 0, 1.5, 3 );
+		this.camera.position.set( 0, 0, 3 );
 		this.camera.lookAt( 0, 0, 0 );
 
 		this.light = new THREE.DirectionalLight();
 		this.light.position.y = 10;
 		this.scene.add( this.light );
 		
-		let geo = new THREE.BoxGeometry( 0.1, 0.1, 0.1 );
+		let geo = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
 		let mat = new THREE.MeshNormalMaterial();
 		this.box = new THREE.Mesh( geo, mat );
 		this.scene.add( this.box );
@@ -34,11 +34,7 @@ export default class MainScene extends ORE.BaseScene {
 
 	animate( deltaTime ) {
 
-		this.box.position.y =  ORE.Easings.easeInOutQuad( Math.min( 1.0, this.time ));
-
-
-		console.log();
-		
+		this.box.position.x =  ORE.Easings.easeInOutQuad( Math.min( 1.0, this.time )) * 1.5 - 0.75;
 		
 		this.renderer.render( this.scene, this.camera );
 
