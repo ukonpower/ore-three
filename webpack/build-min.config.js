e2b27ef00c72ea2769ce332de02d9a0765ec7a0f
ukonpower <obshi0120@icloud.com>
2020-03-23 09:49:55 +0900

    Fixed build setting

--- webpack/build-min.config.js
+++ webpack/build-min.config.js
@@ -9,27 +9,6 @@ module.exports = merge( baseConfig, {
 		library: info.packageBuildName,
 		libraryTarget: 'window'
 	},
-	module: {
-		rules: [
-			{
-				test: /\.ts$/,
-				exclude: /node_modules/,
-				loader: 'ts-loader',
-				options: {
-					configFile: 'webpack/tsconfig/build.json'
-				}
-			},
-			{
-				test: /\.(glsl|vs|fs)$/,
-				loader: 'shader-loader',
-				options: {
-					glsl: {
-						chunkPath: "src/glsl-chunks"
-					}
-				}
-			}
-		]
-	},
 	externals: {
 		'three': 'THREE'
 	},
