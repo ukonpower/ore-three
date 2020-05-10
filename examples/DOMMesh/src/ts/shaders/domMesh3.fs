b1287eae46fd1254c3a3c6df54dfdf44d52edc6b
ukonpower <obshi0120@icloud.com>
2020-05-10 19:00:13 +0900

    rename DOMMesh

---
+++ examples/DOMMesh/src/ts/shaders/domMesh3.fs
@@ -0,0 +1,14 @@
+uniform float time;
+varying vec2 vUv;
+
+void main( void ) {
+	
+	vec3 c = vec3(
+		sin( vUv.x * 10.0 - time * 10.0),
+		sin( vUv.x * 10.0 - time * 10.1 ),
+		sin( vUv.x * 10.0 - time * 10.2 )
+	);
+	
+	gl_FragColor = vec4( c, 1.0 );
+
+}
