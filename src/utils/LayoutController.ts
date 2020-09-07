b2f963afebb26dacd082542bc1df80983eaa108a
ukonpower <obshi0120@icloud.com>
2020-09-07 14:43:26 +0900

    Rename SPLayoutController to LayoutController

---
+++ src/utils/LayoutController.ts
@@ -0,0 +1,59 @@
+import * as THREE from 'three';
+
+export declare interface Transform {
+	position?: THREE.Vector3;
+	rotation?: THREE.Quaternion;
+	scale?: number;
+}
+
+export class LayoutController {
+
+	protected obj: THREE.Object3D;
+	protected baseTransform: Transform;
+	protected baseScale: THREE.Vector3;
+	protected transform: Transform;
+
+	constructor( object: THREE.Object3D, transform: Transform, isAbsolutePosition?: boolean ) {
+
+		this.obj = object;
+
+		this.baseTransform = {
+			position: this.obj.position.clone(),
+			rotation: this.obj.quaternion.clone(),
+		};
+		this.baseScale = this.obj.scale.clone();
+
+		this.transform = transform;
+
+		if ( ! isAbsolutePosition ) {
+
+			this.transform.position && this.transform.position.add( this.obj.position );
+			this.transform.rotation && this.transform.rotation.multiply( this.obj.quaternion );
+
+		}
+
+	}
+
+	public updateTransform( spWeight: number ) {
+
+		if ( this.transform.position ) {
+
+			this.obj.position.copy( this.baseTransform.position.clone().lerp( this.transform.position, spWeight ) );
+
+		}
+
+		if ( this.transform.rotation ) {
+
+			this.obj.quaternion.copy( this.baseTransform.rotation.clone().slerp( this.transform.rotation, spWeight ) );
+
+		}
+
+		if ( this.transform.scale ) {
+
+			this.obj.scale.copy( this.baseScale.clone().multiplyScalar( this.transform.scale * ( spWeight ) + 1.0 - spWeight ) );
+
+		}
+
+	}
+
+}
