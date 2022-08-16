1f5be7f33b94237d7005c8aa2200c0a67b64f15d
ukonpower <ore.ukonpower@gmail.com>
2022-08-16 09:51:00 +0900

    fix: fix test initialization

--- packages/ore-three/tests/core/Controller.test.ts
+++ packages/ore-three/tests/core/Controller.test.ts
@@ -1,4 +1,3 @@
-
 import { BaseLayer } from '../../src/core/BaseLayer';
 import { Controller  } from '../../src/core/Controller'
 
@@ -6,104 +5,99 @@ describe( 'Controller', () => {
 
 	let controller: Controller
 
-	beforeAll( () => {
+	beforeEach( () => {
 
-		controller = new Controller({})
+		controller = new Controller({silent: true})
 
 	} );
 
-	describe( 'Controller', () => {
+	it( 'can add layers', () => {
 
-		it( 'can add layers', () => {
+		controller.addLayer( new BaseLayer( {
+			name: 'testLayer1',
+			context: require('gl')(1, 1)
+		}))
 
-			controller.addLayer( new BaseLayer( {
-				name: 'testLayer1',
-				context: require('gl')(1, 1)
-			}))
+		controller.addLayer( new BaseLayer( {
+			name: 'testLayer2',
+			context: require('gl')(1, 1)
+		}))
 
-			controller.addLayer( new BaseLayer( {
-				name: 'testLayer2',
-				context: require('gl')(1, 1)
-			}))
+		let testLayer1 = controller.getLayer( 'testLayer1' )
+		let testLayer2 = controller.getLayer( 'testLayer2' )
 
-			let testLayer1 = controller.getLayer( 'testLayer1' )
-			let testLayer2 = controller.getLayer( 'testLayer2' )
+		expect(testLayer1).not.toBeNull()
+		expect(testLayer2).not.toBeNull()
 
-			expect(testLayer1).not.toBeNull()
-			expect(testLayer2).not.toBeNull()
+		if( testLayer1 ) {
 
-			if( testLayer1 ) {
-
-				expect(testLayer1.info.name).toEqual( 'testLayer1' )
-				
-			}
-
-			if( testLayer2 ) {
-
-				expect(testLayer2.info.name).toEqual( 'testLayer2' )
-				
-			}
-
-		} );
-
-		it( 'can remove layers', () => {
-
-			controller.addLayer( new BaseLayer( {
-				name: 'testLayer1',
-				context: require('gl')(1, 1)
-			}))
-
-			controller.addLayer( new BaseLayer( {
-				name: 'testLayer2',
-				context: require('gl')(1, 1)
-			}))
-
-			controller.removeLayer( 'testLayer1' );
+			expect(testLayer1.info.name).toEqual( 'testLayer1' )
 			
-			let testLayer1 = controller.getLayer( 'testLayer1' )
-			let testLayer2 = controller.getLayer( 'testLayer2' )
+		}
+
+		if( testLayer2 ) {
+
+			expect(testLayer2.info.name).toEqual( 'testLayer2' )
 			
-			expect(testLayer1).toBeNull()
-			expect(testLayer2).not.toBeNull()
-			
-		} );
+		}
 
-		it( 'can dispose', () => {
+	} );
 
-			controller.addLayer( new BaseLayer( {
-				name: 'testLayer',
-				context: require('gl')(1, 1)
-			}))
+	it( 'can remove layers', () => {
 
-			controller.dispose()
+		controller.addLayer( new BaseLayer( {
+			name: 'testLayer1',
+			context: require('gl')(1, 1)
+		}))
 
-			let testLayer = controller.getLayer( 'testLayer' );
-			
-			expect(testLayer).toBeNull()
+		controller.addLayer( new BaseLayer( {
+			name: 'testLayer2',
+			context: require('gl')(1, 1)
+		}))
 
-		} );
+		controller.removeLayer( 'testLayer1' );
 		
-		it( 'can set pointer event element', () => {
+		let testLayer1 = controller.getLayer( 'testLayer1' )
+		let testLayer2 = controller.getLayer( 'testLayer2' )
+		
+		expect(testLayer1).toBeNull()
+		expect(testLayer2).not.toBeNull()
+		
+	} );
 
-			let elm1 = document.createElement('div');
-			let elm2 = document.createElement('div');
+	it( 'can dispose', () => {
 
-			// constructor
-			
-			let controller = new Controller({
-				pointerEventElement: elm1
-			})
+		controller.addLayer( new BaseLayer( {
+			name: 'testLayer',
+			context: require('gl')(1, 1)
+		}))
 
-			expect(elm1.isEqualNode( controller.pointer.element) ).toBeTruthy()
-			
-			// method
+		controller.dispose()
 
-			controller.setPointerEventElement( elm2 );
+		let testLayer = controller.getLayer( 'testLayer' );
+		
+		expect(testLayer).toBeNull()
 
-			expect(elm2.isEqualNode( controller.pointer.element) ).toBeTruthy()
+	} );
+	
+	it( 'can set pointer event element', () => {
 
+		let elm1 = document.createElement('div');
+		let elm2 = document.createElement('div');
 
-		} );
+		// constructor
+		
+		let controller = new Controller({
+			pointerEventElement: elm1
+		})
+
+		expect(elm1.isEqualNode( controller.pointer.element) ).toBeTruthy()
+		
+		// method
+
+		controller.setPointerEventElement( elm2 );
+
+		expect(elm2.isEqualNode( controller.pointer.element) ).toBeTruthy()
 
 	} );
 
