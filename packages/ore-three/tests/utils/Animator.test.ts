c7a0ed8b62d75e127edd040bb18f75350d9c9ea8
ukonpower <ore.ukonpower@gmail.com>
2024-01-09 20:40:51 +0900

    test

--- packages/ore-three/tests/utils/Animator.test.ts
+++ packages/ore-three/tests/utils/Animator.test.ts
@@ -5,7 +5,7 @@ import * as THREE from 'three';
 import { Animator } from '../../src/utils/Animation/Animator';
 import { Easings } from '../../src/utils/Animation/Easings';
 
-describe( 'Controller', () => {
+describe( 'Animator', () => {
 
 	let animator: Animator;
 
@@ -20,7 +20,7 @@ describe( 'Controller', () => {
 		animator.add( {
 			name: 'number',
 			initValue: 0,
-			easing: Easings.easeInCubic
+			easing: Easings.easeInCubic,
 		} );
 
 		animator.add( {
@@ -58,6 +58,157 @@ describe( 'Controller', () => {
 		expect( varVector3?.isVector3 ).toBeTruthy();
 		expect( varQuaternion?.isQuaternion ).toBeTruthy();
 		expect( Array.isArray( varArray ) ).toBeTruthy();
+
+	} );
+
+	it( 'can set easing', () => {
+
+		animator.add( {
+			name: 'number',
+			initValue: 0,
+			easing: Easings.easeInCubic,
+		} );
+
+		animator.setEasing( 'number', Easings.easeOutCubic );
+
+		const variable = animator.getVariableObject<number>( 'number' );
+		expect( variable?.easing ).toBe( Easings.easeOutCubic );
+
+	} );
+
+	it( 'can set value', () => {
+
+		animator.add( {
+			name: 'number',
+			initValue: 0,
+			easing: Easings.easeInCubic,
+		} );
+
+		animator.setValue( 'number', 10 );
+
+		const variable = animator.getVariableObject<number>( 'number' );
+		expect( variable?.value ).toBe( 10 );
+
+	} );
+
+	it( 'can animate', () => {
+
+		animator.add( {
+			name: 'number',
+			initValue: 0,
+			easing: Easings.easeInCubic,
+		} );
+
+		animator.animate( 'number', 100, 1 );
+
+		const variable = animator.getVariableObject<number>( 'number' );
+		expect( variable?.isAnimating ).toBeTruthy();
+		expect( variable?.goalValue ).toBe( 100 );
+
+	} );
+
+	it( 'can cancel animate', () => {
+
+		animator.add( {
+			name: 'number',
+			initValue: 0,
+			easing: Easings.easeInCubic,
+		} );
+
+		animator.animate( 'number', 100, 1 );
+		animator.cancelAnimate( 'number' );
+
+		const variable = animator.getVariableObject<number>( 'number' );
+		expect( variable?.isAnimating ).toBeFalsy();
+		expect( variable?.time ).toBe( - 1.0 );
+
+	} );
+
+	it( 'can get variable', () => {
+
+		animator.add( {
+			name: 'number',
+			initValue: 0,
+			easing: Easings.easeInCubic,
+		} );
+
+		const variable = animator.getVariableObject<number>( 'number' );
+		expect( variable?.value ).toBe( 0 );
+
+	} );
+
+	it( 'can get variable object', () => {
+
+		animator.add( {
+			name: 'number',
+			initValue: 0,
+			easing: Easings.easeInCubic,
+		} );
+
+		const variable = animator.getVariableObject<number>( 'number' );
+		expect( variable?.value ).toBe( 0 );
+
+	} );
+
+	it( 'can apply to uniforms', () => {
+
+		const uniforms: any = {};
+		animator.add( {
+			name: 'number',
+			initValue: 0,
+			easing: Easings.easeInCubic,
+		} );
+
+		animator.applyToUniforms( uniforms );
+
+		expect( uniforms.number ).toBeDefined();
+		expect( uniforms.number.value ).toBe( 0 );
+
+	} );
+
+	it( 'can check if animating', () => {
+
+		animator.add( {
+			name: 'number',
+			initValue: 0,
+			easing: Easings.easeInCubic,
+		} );
+
+		animator.animate( 'number', 100, 1 );
+
+		expect( animator.isAnimating() ).toBeTruthy();
+		expect( animator.isAnimating( 'number' ) ).toBeTruthy();
+
+	} );
+
+	it( 'can wait', async () => {
+
+		const startTime = Date.now();
+		const waitTime = 1; // seconds
+
+		await animator.wait( waitTime );
+
+		const endTime = Date.now();
+		const elapsedTime = ( endTime - startTime ) / 1000; // seconds
+
+		expect( elapsedTime ).toBeGreaterThanOrEqual( waitTime );
+
+	} );
+
+	it( 'can update', () => {
+
+		animator.add( {
+			name: 'number',
+			initValue: 0,
+			easing: Easings.easeInCubic,
+		} );
+
+		animator.animate( 'number', 100, 1 );
+
+		animator.update( 0.5 );
+
+		const variable = animator.getVariableObject<number>( 'number' );
+		expect( variable?.value ).toBeGreaterThan( 0 );
 
 	} );
 
---
+++ packages/ore-three/tests/utils/Lerps.test.ts
@@ -0,0 +1,86 @@
+/* eslint no-undef: 0 */
+
+import * as THREE from 'three';
+import { Animator } from '../../src/utils/Animation/Animator';
+import { Lerps } from '../../src/utils/Lerps';
+
+describe( 'Lerps', () => {
+
+	let animator: Animator;
+
+	beforeEach( () => {
+
+		animator = new Animator();
+
+	} );
+
+	it( 'can lerp numbers', () => {
+
+		const a = 0;
+		const b = 10;
+		const t = 0.5;
+		const result = Lerps.number( a, b, t );
+		expect( result ).toBe( 5 );
+
+	} );
+
+	it( 'can lerp number arrays', () => {
+
+		const a = [ 0, 0, 0 ];
+		const b = [ 10, 10, 10 ];
+		const t = 0.5;
+		const result = Lerps.numberArray( a, b, t );
+		expect( result ).toEqual( [ 5, 5, 5 ] );
+
+	} );
+
+	it( 'can lerp THREE vectors', () => {
+
+		const a = new THREE.Vector3( 0, 0, 0 );
+		const b = new THREE.Vector3( 10, 10, 10 );
+		const t = 0.5;
+		const result = Lerps.THREEVectors( a, b, t );
+		expect( result ).toEqual( new THREE.Vector3( 5, 5, 5 ) );
+
+	} );
+
+	it( 'can slerp THREE quaternions', () => {
+
+		const a = new THREE.Quaternion();
+		const b = new THREE.Quaternion().setFromAxisAngle( new THREE.Vector3( 0, 1, 0 ), Math.PI );
+		const t = 0.5;
+		const result = Lerps.THREEQuaternion( a, b, t );
+		expect( result ).toContainEqual( new THREE.Quaternion().setFromAxisAngle( new THREE.Vector3( 0, 1, 0 ), Math.PI / 2 ) );
+
+	} );
+
+	it( 'can lerp THREE euler angles', () => {
+
+		const a = new THREE.Euler( 0, 0, 0 );
+		const b = new THREE.Euler( Math.PI, Math.PI, Math.PI );
+		const t = 0.5;
+		const result = Lerps.THREEEuler( a, b, t );
+		expect( result ).toEqual( new THREE.Euler( Math.PI / 2, Math.PI / 2, Math.PI / 2 ) );
+
+	} );
+
+	it( 'can get lerp function based on variable type', () => {
+
+		const numberLerpFunc = Lerps.getLerpFunc( 0 );
+		expect( numberLerpFunc ).toBe( Lerps.number );
+
+		const arrayLerpFunc = Lerps.getLerpFunc( [ 0, 0, 0 ] );
+		expect( arrayLerpFunc ).toBe( Lerps.numberArray );
+
+		const vectorLerpFunc = Lerps.getLerpFunc( new THREE.Vector3() );
+		expect( vectorLerpFunc ).toBe( Lerps.THREEVectors );
+
+		const quaternionLerpFunc = Lerps.getLerpFunc( new THREE.Quaternion() );
+		expect( quaternionLerpFunc ).toBe( Lerps.THREEQuaternion );
+
+		const eulerLerpFunc = Lerps.getLerpFunc( new THREE.Euler() );
+		expect( eulerLerpFunc ).toBe( Lerps.THREEEuler );
+
+	} );
+
+} );
